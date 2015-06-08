9cb45ff4a93e195d082affb11b1e8225254c50fe
Christian Fobel <christian@fobel.net>
2015-06-08 07:44:35 -0400

    Add numpy array interface to findHomography
    Add `numpy` array interface to OpenCV perspective transform functions.

--- opencv_helpers/__init__.py
+++ opencv_helpers/__init__.py
@@ -1,4 +1,4 @@
-from .safe_cv import cv
+from .safe_cv import cv, cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -49,3 +49,23 @@ def convert_color(im):
     converted = cv.CreateImage((im.width, im.height), 8, im.channels)
     cv.CvtColor(im, converted, cv.CV_BGR2RGB)
     return converted
+
+
+def get_map_array(df_a, df_b):
+    map_mat = cv.CreateMat(3, 3, cv.CV_32FC1)
+    cv.GetPerspectiveTransform(map(tuple, df_a.values),
+                               map(tuple, df_b.values), map_mat)
+    return np.fromstring(map_mat.tostring(), dtype='f32').reshape(3, 3)
+
+
+def find_homography_array(df_a, df_b):
+    map_arr, mask = cv2.findHomography(df_a.astype(float), df_b.astype(float))
+    return map_arr
+
+
+def cvwarp_mat_to_4x4(warp_arr):
+    warp_arr4x4 = np.identity(4, dtype='f32')
+    warp_arr4x4[:2, :2] = warp_arr[:2, :2]
+    warp_arr4x4[-1, :2] = warp_arr[-1, :2]
+    warp_arr4x4[:2, -1] = warp_arr[:2, -1]
+    return warp_arr4x4
