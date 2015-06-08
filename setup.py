e1bf8f8af48d55745623477b11b3e3ef31e44d0c
Christian Fobel <christian@fobel.net>
2015-06-08 07:52:33 -0400

    Add version script

--- setup.py
+++ setup.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python
-
-import distutils.core
-
 try:
-    from distutils.command.build_py import build_py_2to3 as build_py
+    from setuptools import setup
 except ImportError:
-    from distutils.command.build_py import build_py
+    from distutils.core import setup
 
-# Setup script for path
-
-kw = {
-    'name': "opencv_helpers",
-    'version': "1.0",
-    'description': 'Helper functions for opencv',
-    'author': "Christian Fobel",
-    'author_email': "christian@fobel.net",
-    'url': "https://github.com/cfobel/python___opencv_examples",
-    'license': "GPLv2 License",
-    'packages': ['opencv_helpers'],
-    'cmdclass': dict(build_py=build_py),
-    'package_data': {'opencv_helpers': ['statepy/*.py', 'statepy/test/*',
-                            'cvwin/*', 'glade/*']}
-}
+import sys
+sys.path.insert(0, '.')
+import version
 
 
-# If we're running Python 2.3, add extra information
-if hasattr(distutils.core, 'setup_keywords'):
-    if 'classifiers' in distutils.core.setup_keywords:
-        kw['classifiers'] = [
-            'Development Status :: 4 - Beta',
-            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-            'Intended Audience :: Developers',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Software Development :: Libraries :: Python Modules'
-          ]
-    if 'download_url' in distutils.core.setup_keywords:
-        urlfmt = "https://github.com/cfobel/python___opencv_examples/tarball/%s"
-        kw['download_url'] = urlfmt % kw['version']
-
-
-distutils.core.setup(**kw)
+setup(name="opencv_helpers",
+      version=version.getVersion(),
+      description='Helper functions for opencv',
+      author="Christian Fobel",
+      author_email="christian@fobel.net",
+      url="https://github.com/cfobel/python___opencv_examples",
+      license="GPLv2 License",
+      packages=['opencv_helpers'],
+      package_data={'opencv_helpers': ['statepy/*.py', 'statepy/test/*',
+                                       'cvwin/*', 'glade/*']})
